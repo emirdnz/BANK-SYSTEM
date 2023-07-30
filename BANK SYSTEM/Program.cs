1f4350cc045c3926be22a3d27a31083c68cf71ea
polegut <polegut8@gmail.com>
2023-07-30 06:47:04 +0300

    Add project files.

---
+++ BANK SYSTEM/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BANK_SYSTEM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
